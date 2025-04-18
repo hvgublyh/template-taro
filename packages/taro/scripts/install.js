3103d1c7108003f7e40fc3200365b58a974e6ce3
hvgu <hvgublyh@outlook.com>
2025-04-18 19:08:37 +0800

    feat: 删除依赖命令修改

--- packages/taro/scripts/install.js
+++ packages/taro/scripts/install.js
@@ -7,7 +7,6 @@ const rl = readline.createInterface({
 });
 
 rl.question('请输入依赖: ', (name) => {
-  console.log(`${name}安装中...`);
   const npmInstall = spawn('pnpm.cmd', ['i', '--save', `${name}`], { stdio: 'inherit' });
   npmInstall.on('close', (code) => {
     console.log(`npm install 进程退出，代码: ${code}`);
